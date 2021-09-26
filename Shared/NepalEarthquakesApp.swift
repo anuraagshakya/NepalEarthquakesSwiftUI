d0f8e21fbfdb2cb8ab75fb2a794773b156f2c4ff
Anuraag Shakya <anuraag.shakya@gmail.com>
2021-09-26 14:27:18 +0200

    Initial Commit

---
+++ Shared/NepalEarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NepalEarthquakesApp.swift
+//  Shared
+//
+//  Created by Anuraag on 26.09.21.
+//
+
+import SwiftUI
+
+@main
+struct NepalEarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
